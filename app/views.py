8d15e9bd459d97f04d5f06b44126545db1295471
koffskeyj <koffskeyj@gmail.com>
2016-09-15 15:14:02 -0400

    have no idea what is going on

--- app/views.py
+++ app/views.py
@@ -148,7 +148,7 @@ class FootballLocationListView(LoginRequiredMixin, ListView):
             team_info = team_list.append(team["href"].split("/"))
         team_dict = {team[8]: team[7] for team in team_list}
         team_dict["miami-redhawks"] = team_dict.pop("miami-(oh)-redhawks")
-        team_dict["loyola-(md)-greyhounds"] = team_dict.pop("loyola-greyhounds")
+        team_dict["loyola-greyhounds"] = team_dict.pop("loyola-(md)-greyhounds")
         print(team_dict)
         format_user_team = user_team.lower().replace(" ", "-").replace("&", "%26").replace("é", "%C3%A9").replace("st.", "st")
         format_user_team = re.sub('\(.+?\)', '', format_user_team).replace("--", "-")
