c912c2d2a4c5408ae70520ddd18e5eb93dd69eb0
koffskeyj <koffskeyj@gmail.com>
2017-03-08 17:38:58 -0500

    More data changes

--- app/views.py
+++ app/views.py
@@ -146,6 +146,7 @@ class FootballLocationListView(LoginRequiredMixin, ListView):
         team_id = souper.find_all(class_="bi")
         for team in team_id:
             team_info = team_list.append(team["href"].split("/"))
+        print(team_info)
         team_dict = {team[8]: team[7] for team in team_list}
         team_dict["miami-redhawks"] = team_dict.pop("miami-(oh)-redhawks")
         print(team_dict)
@@ -186,11 +187,12 @@ class BasketballLocationListView(LoginRequiredMixin, ListView):
         team_id = souper.find_all(class_="bi")
         for team in team_id:
             team_info = team_list.append(team["href"].split("/"))
+        print(team_info)
         team_dict = {team[8]: team[7] for team in team_list}
         team_dict["miami-redhawks"] = team_dict.pop("miami-(oh)-redhawks")
         team_dict["st-francis-terriers"] = team_dict.pop("st-francis-(bkn)-terriers")
         team_dict["st-francis-red-flash"] = team_dict.pop("st-francis-(pa)-red-flash")
-        team_dict["loyola-chicago-ramblers"] = team_dict.pop("loyola-(chi)-ramblers")
+        # team_dict["loyola-chicago-ramblers"] = team_dict.pop("loyola-(chi)-ramblers")
         team_dict["loyola-md-ramblers"] = team_dict.pop("loyola-(md)-greyhounds")
         print(team_dict)
         format_user_team = user_team.lower().replace(" ", "-").replace("&", "%26").replace("é", "%C3%A9").replace("st.", "st").replace("brooklyn", "")
